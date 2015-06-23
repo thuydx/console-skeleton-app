83c3f9ea274686dc665d87b7834ed4f1e370f27d
Gianluca Arbezzano <gianarb92@gmail.com>
2015-06-24 00:49:46 +0200

    Fixed #2

--- config/routes.php
+++ config/routes.php
@@ -1,8 +1,11 @@
- <?php return [
-    [
-        'name' => 'hello',
-        'route' => "--name=",
-        'short_description' => "Good morning!! This is a beautiful day",
-        "handler" => ['App\Command\Hello', 'run'],
-    ],
+<?php
+return [
+    'routes' => [
+        [
+            'name' => 'hello',
+            'route' => "--name=",
+            'short_description' => "Good morning!! This is a beautiful day",
+            "handler" => ['App\Command\Hello', 'run'],
+        ],
+    ]
 ];
